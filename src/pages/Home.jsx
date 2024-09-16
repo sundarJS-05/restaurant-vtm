46e85a1b7eb4fc5fb3f745dbd6ecc2564be0f534
sundarJS-05 <sundar00505@gmail.com>
2024-09-17 00:26:51 +0530

    most of it done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Home = () => {
+  return (
+      <Layout>
+        Home page.
+        </Layout>
+  )
+}
+
+export default Home
