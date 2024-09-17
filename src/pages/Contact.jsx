36de05ee971ab8cf79d48ab8748e4d709d7c87f9
sundarJS-05 <sundar00505@gmail.com>
2024-09-17 11:33:13 +0530

    nearing completion

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,28 @@
 
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import { Box, Typography } from '@mui/material'
 
 const Contact = () => {
   return (
        <Layout>
-        Contact
+         <Box sx={{
+            mx:3, my:8
+          }}>
+            <Typography variant='h5'>
+
+              <h3> Contact details. </h3>
+
+              <br></br>
+
+              <p>
+              Random contact details;  since this does NOT repsresent a real restaurant
+              </p>
+            
+            </Typography>
+
+         </Box>
+
         </Layout>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,34 @@
 
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import {Link} from 'react-router-dom'
+import meals from '../images/meals.jpeg'
+import '../styles/HomeStyles.css'
+
 
 const Home = () => {
   return (
       <Layout>
-        Home page.
+        <div className='home' style={{backgroundImage:`${meals}`}}>
+          <div className="headerContainer">
+            <h3>
+              Restaurant-vmt -- site created by Sundararajan
+            </h3>
+
+            <p>
+              All food items and content here are only for fictional purposes;
+            </p>
+
+            <br></br>
+
+            <Link to='/menu'>
+            <button> Go to MENU page... </button>
+            </Link>
+
+          </div>
+
+        </div>
+  
         </Layout>
   )
 }
